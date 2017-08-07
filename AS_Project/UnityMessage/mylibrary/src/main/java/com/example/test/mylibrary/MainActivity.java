5a2e542f1348932644c73181275207aad4329d4f
Tom Tang <997063802@qq.com>
2017-08-08 07:16:15 +0800

    Create Project

---
+++ AS_Project/UnityMessage/mylibrary/src/main/java/com/example/test/mylibrary/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test.mylibrary;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends UnityPlayerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public String ShowDialog(final String _title, final String _content){
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(_title).setMessage(_content).setPositiveButton("确定", null).setNegativeButton("取消", null);
+                builder.show();
+            }
+        });
+
+        return "Android Dialog";
+    }
+
+
+
+}
---
+++ Assets/Scripts/TestAndroid.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class TestAndroid : MonoBehaviour {
+
+	public Text label;
+
+	public void OnClick() {  
+		AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+		string[] str = new string[2] {
+			"HelloWorld",
+			"Show Android Dialog!"
+		};  
+		string ret = jo.Call<string>("ShowDialog", str);
+		label.text = ret;
+	}
+
+
+}
